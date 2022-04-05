ffe2a44c11b9ad641a9ad1c3f17e932dc2ce9904
i-naeem <inaeem@protonmail.com>
2022-04-05 10:23:41 +0500

    :card_box_files: connect database

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
-import cookieParser from 'cookie-parser';
 import indexRouter from './routes/index.js';
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+import { config } from 'dotenv';
 import express from 'express';
 import logger from 'morgan';
 import path from 'path';
 
+// Load Env
+config();
+
 const app = express();
+
+mongoose.connect(process.env.DATABASE_URL, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log('Database Connected');
+  }
+});
 
 const PUBLIC_DIRECTORY = path.resolve('.', 'public');
 
